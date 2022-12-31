9a5baeed7835db2b366933bb290ac66c1e6d18fb
c <94270715+tpcav@users.noreply.github.com>
2022-12-30 21:52:37 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 
 type Word = {
   word: string
@@ -13,43 +13,58 @@ const WordsPage = () => {
   const [searchTerm1, setSearchTerm1] = useState('');
   const [searchTerm2, setSearchTerm2] = useState('');
 
-  const handleRelatedSubmit = async (event: any) => {
-    event.preventDefault();
+  useEffect(() => {
+    const fetchRelatedWords = async () => {
+      const res = await fetch(
+        `https://api.datamuse.com/words?ml=${searchTerm}`
+      );
+      const data = await res.json();
+      setRelatedWords(data);
+    };
 
-    const res = await fetch(
-      `https://api.datamuse.com/words?ml=${searchTerm}`
-    );
-    const data = await res.json();
-    setRelatedWords(data);
+    const fetchRhymingWords = async () => {
+      const res = await fetch(
+        `https://api.datamuse.com/words?rel_rhy=${searchTerm1}`
+      );
+      const data = await res.json();
+      setRhymingWords(data);
+    };
+
+    const fetchAdjWords = async () => {
+      const res = await fetch(
+        `https://api.datamuse.com/words?rel_jjb=${searchTerm2}`
+      );
+      const data = await res.json();
+      setAdjWords(data);
+    };
+
+    fetchRelatedWords();
+    fetchRhymingWords();
+    fetchAdjWords();
+  }, [searchTerm, searchTerm1, searchTerm2]);
+
+  const handleRelatedSubmit = (event: any) => {
+    event.preventDefault();
+    setSearchTerm(event.target.value);
   };
 
-  const handleRhymingSubmit = async (event: any) => {
+  const handleRhymingSubmit = (event: any) => {
     event.preventDefault();
-
-    const res = await fetch(
-      `https://api.datamuse.com/words?rel_rhy=${searchTerm1}`
-    );
-    const data = await res.json();
-    setRhymingWords(data);
+    setSearchTerm1(event.target.value);
   };
 
-  const handleAdjSubmit = async (event: any) => {
+  const handleAdjSubmit = (event: any) => {
     event.preventDefault();
-
-    const res = await fetch(
-      `https://api.datamuse.com/words?rel_jjb=${searchTerm2}`
-    );
-    const data = await res.json();
-    setAdjWords(data);
+    setSearchTerm2(event.target.value);
   };
 
   return (
     <div className="grid p-4">
       <Head>
-        <title>DomainTLDR - Word Generator</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name='description' content='Word Generator'/>
-      </Head>
+        <title>DomainTLDR - Word Generator</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='description' content='Word Generator'/>
+      </Head>
       <div className='p-4 pb-10'>
         <h1 className='text-2xl font-bold mb-4'>DomainTLDR</h1>
         <p className='text-center mt-10 mb-4 italic'>Are your names for ideas too long?</p>
@@ -57,7 +72,7 @@ const WordsPage = () => {
       </div>
 
       <h1 className="text-lg font-bold text-center">Related Words</h1>
-      <form onSubmit={handleRelatedSubmit} className="p-4 text-center">
+            <form onSubmit={handleRelatedSubmit} className="p-4 text-center">
         <input
           type="text"
           placeholder="Enter search term"
@@ -117,9 +132,7 @@ const WordsPage = () => {
         ))}
       </div>
     </div>
-    </>
   );
 };
 
 export default WordsPage;
-
