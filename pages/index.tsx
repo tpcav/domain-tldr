fd203622817a4d0cca9fd7abc6cecd078af5790f
c <thomascavalcante77@gmail.com>
2022-12-31 13:37:53 -0500

    search fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,79 +1,104 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
+import Nav from '../dashboard/Nav';
 
-type Word = {
+  type Word = {
   word: string
-};
+  };
 
-const WordsPage = () => {
-  const [relatedWords, setRelatedWords] = useState<Word[]>([]);
-  const [rhymingWords, setRhymingWords] = useState<Word[]>([]);
-  const [adjWords, setAdjWords] = useState<Word[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchTerm1, setSearchTerm1] = useState('');
-  const [searchTerm2, setSearchTerm2] = useState('');
+  const WordsPage = () => {
+    const [relatedWords, setRelatedWords] = useState<Word[]>([]);
+    const [rhymingWords, setRhymingWords] = useState<Word[]>([]);
+    const [adjWords, setAdjWords] = useState<Word[]>([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm1, setSearchTerm1] = useState('');
+    const [searchTerm2, setSearchTerm2] = useState('');
+    const [showRelatedWords, setShowRelatedWords] = useState(false);
+    const [showRhymingWords, setShowRhymingWords] = useState(false);
+    const [showAdjWords, setShowAdjWords] = useState(false);
 
-  useEffect(() => {
-    const fetchRelatedWords = async () => {
-      const res = await fetch(
-        `https://api.datamuse.com/words?ml=${searchTerm}`
-      );
-      const data = await res.json();
-      setRelatedWords(data);
-    };
+  const fetchRelatedWords = async () => {
+    const res = await fetch(
+      `https://api.datamuse.com/words?ml=${searchTerm}`
+    );
+    const data = await res.json();
+    setRelatedWords(data);
+  };
 
-    const fetchRhymingWords = async () => {
-      const res = await fetch(
-        `https://api.datamuse.com/words?rel_rhy=${searchTerm1}`
-      );
-      const data = await res.json();
-      setRhymingWords(data);
-    };
+  const fetchRhymingWords = async () => {
+    const res = await fetch(
+      `https://api.datamuse.com/words?rel_rhy=${searchTerm1}`
+    );
+    const data = await res.json();
+    setRhymingWords(data);
+  };
 
-    const fetchAdjWords = async () => {
-      const res = await fetch(
-        `https://api.datamuse.com/words?rel_jjb=${searchTerm2}`
-      );
-      const data = await res.json();
-      setAdjWords(data);
-    };
-
-    fetchRelatedWords();
-    fetchRhymingWords();
-    fetchAdjWords();
-  }, [searchTerm, searchTerm1, searchTerm2]);
+  const fetchAdjWords = async () => {
+    const res = await fetch(
+      `https://api.datamuse.com/words?rel_jjb=${searchTerm2}`
+    );
+    const data = await res.json();
+    setAdjWords(data);
+  };
 
   const handleRelatedSubmit = (event: any) => {
     event.preventDefault();
-    setSearchTerm(event.target.value);
+    setSearchTerm(event.currentTarget.elements.search.value);
+    fetchRelatedWords();
+    setShowRelatedWords(true);
   };
 
   const handleRhymingSubmit = (event: any) => {
     event.preventDefault();
-    setSearchTerm1(event.target.value);
+    setSearchTerm1(event.currentTarget.elements.search.value);
+    fetchRhymingWords();
+    setShowRhymingWords(true);
   };
 
   const handleAdjSubmit = (event: any) => {
     event.preventDefault();
-    setSearchTerm2(event.target.value);
+    setSearchTerm2(event.currentTarget.elements.search.value);
+    fetchAdjWords();
+    setShowAdjWords(true);
   };
 
   return (
-    <div className="grid p-4">
+    <div className="items-center justify-center bg-gray-100 mt-20 h-screen">
       <Head>
-        <title>DomainTLDR - Word Generator</title>
+        <title>Word Generator for Ideas</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name='description' content='DomainTLDR. Word Generator'/>
       </Head>
+
+
+      <nav className="bg-white shadow-md fixed top-0 w-full">
+        <div className="container mx-auto p-6 flex py-2 items-center justify-between">
+          <Link href="/" className="font-bold text-2xl text-gray-900">DomainTLDR</Link>
+          <ul className="md:flex md:items-center">
+            <li className="mr-6">
+              <Link href="/" className="text-gray-600 hover:text-gray-900">Home</Link>
+            </li>
+            <li className="mr-6">
+              <Link href="/about" className="text-gray-600 hover:text-gray-900">About</Link>
+            </li>
+            <li className="mr-6">
+              <Link href="mailto:cavalcantethomas@icloud.com" className="text-gray-600 hover:text-gray-900">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
       <div className='p-4 pb-10'>
-        <h1 className='text-2xl font-bold mb-4'>DomainTLDR</h1>
-        <p className='text-center mt-10 mb-4 italic'>Are your names for ideas too long?</p>
-        <p className='text-center'>Use our tools to make them short and memorable.</p>
+        <p className='text-center mt-10 mb-4 italic'>Stuck coming up with words?</p>
+        <p className='text-center'>Use the tools below to generate words for ideas.</p>
       </div>
 
-      <h1 className="text-lg font-bold text-center">Related Words</h1>
-            <form onSubmit={handleRelatedSubmit} className="p-4 text-center">
+
+      <h1 className="mt-5 text-lg font-bold text-center">Related Words</h1>
+        <form onSubmit={handleRelatedSubmit} className="text-center mb-10">
         <input
+          name="search"
           type="text"
           placeholder="Enter search term"
           value={searchTerm}
@@ -84,16 +109,20 @@ const WordsPage = () => {
           Search
         </button>
       </form>
-      <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4">
-        {relatedWords.map((word: Word) => (
-          <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
-            {word.word}
-          </div>
-        ))}
-      </div>
-      <h1 className="text-lg font-bold text-center mt-4">Rhyming Words</h1>
-      <form onSubmit={handleRhymingSubmit} className="p-4 text-center">
+      {showRelatedWords && (
+        <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4 p-2">
+          {relatedWords.map((word: Word) => (
+            <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
+              {word.word}
+            </div>
+          ))}
+        </div>
+      )}
+
+      <h1 className="text-lg font-bold text-center">Rhyming Words</h1>
+        <form onSubmit={handleRhymingSubmit} className="text-center mb-10">
         <input
+          name="search"
           type="text"
           placeholder="Enter search term"
           value={searchTerm1}
@@ -104,16 +133,21 @@ const WordsPage = () => {
           Search
         </button>
       </form>
-      <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4">
-        {rhymingWords.map((word: Word) => (
-          <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
-            {word.word}
-          </div>
-        ))}
-      </div>
-      <h1 className="text-lg font-bold text-center mt-4">Adjective Words</h1>
-      <form onSubmit={handleAdjSubmit} className="p-4 text-center">
+      {showRhymingWords && (
+        <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4 p-2">
+          {rhymingWords.map((word: Word) => (
+            <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
+              {word.word}
+            </div>
+          ))}
+        </div>
+      )}
+
+
+      <h1 className="text-lg font-bold text-center">Adjective Words</h1>
+        <form onSubmit={handleAdjSubmit} className="text-center mb-10">
         <input
+          name="search"
           type="text"
           placeholder="Enter search term"
           value={searchTerm2}
@@ -124,13 +158,19 @@ const WordsPage = () => {
           Search
         </button>
       </form>
-      <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4">
-        {adjWords.map((word: Word) => (
-          <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
-            {word.word}
-          </div>
-        ))}
-      </div>
+      {showAdjWords && (
+        <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4 p-2">
+          {adjWords.map((word: Word) => (
+            <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
+              {word.word}
+            </div>
+          ))}
+        </div>
+      )}
+
+      <footer className='text-center mt-24'>Made with
+        <Link href='https://www.datamuse.com/api/' className='font-medium text-blue-600 dark:text-blue-500 hover:underline'> Datamuse API</Link>
+      </footer>
     </div>
   );
 };
---
+++ pages/test.tsx
@@ -0,0 +1,138 @@
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+
+type Word = {
+  word: string
+};
+
+const WordsPage = () => {
+  const [relatedWords, setRelatedWords] = useState<Word[]>([]);
+  const [rhymingWords, setRhymingWords] = useState<Word[]>([]);
+  const [adjWords, setAdjWords] = useState<Word[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm1, setSearchTerm1] = useState('');
+  const [searchTerm2, setSearchTerm2] = useState('');
+
+  useEffect(() => {
+    const fetchRelatedWords = async () => {
+      const res = await fetch(
+        `https://api.datamuse.com/words?ml=${searchTerm}`
+      );
+      const data = await res.json();
+      setRelatedWords(data);
+    };
+
+    const fetchRhymingWords = async () => {
+      const res = await fetch(
+        `https://api.datamuse.com/words?rel_rhy=${searchTerm1}`
+      );
+      const data = await res.json();
+      setRhymingWords(data);
+    };
+
+    const fetchAdjWords = async () => {
+      const res = await fetch(
+        `https://api.datamuse.com/words?rel_jjb=${searchTerm2}`
+      );
+      const data = await res.json();
+      setAdjWords(data);
+    };
+
+    fetchRelatedWords();
+    fetchRhymingWords();
+    fetchAdjWords();
+  }, [searchTerm, searchTerm1, searchTerm2]);
+
+  const handleRelatedSubmit = (event: any) => {
+    event.preventDefault();
+    setSearchTerm(event.target.value);
+  };
+
+  const handleRhymingSubmit = (event: any) => {
+    event.preventDefault();
+    setSearchTerm1(event.target.value);
+  };
+
+  const handleAdjSubmit = (event: any) => {
+    event.preventDefault();
+    setSearchTerm2(event.target.value);
+  };
+
+  return (
+    <div className="grid p-4">
+      <Head>
+        <title>DomainTLDR - Word Generator</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='description' content='DomainTLDR. Word Generator'/>
+      </Head>
+      <div className='p-4 pb-10'>
+        <h1 className='text-2xl font-bold mb-4'>DomainTLDR</h1>
+        <p className='text-center mt-10 mb-4 italic'>Are your names for ideas too long?</p>
+        <p className='text-center'>Use our tools to make them short and memorable.</p>
+      </div>
+
+      <h1 className="text-lg font-bold text-center">Related Words</h1>
+        <form onSubmit={handleRelatedSubmit} className="p-4 text-center">
+        <input
+          type="text"
+          placeholder="Enter search term"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          className="bg-gray-200 rounded p-2"
+        />
+        <button type="submit" className="m-2 p-2 bg-blue-200 rounded-lg">
+          Search
+        </button>
+      </form>
+      <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4">
+        {relatedWords.map((word: Word) => (
+          <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
+            {word.word}
+          </div>
+        ))}
+      </div>
+      <h1 className="text-lg font-bold text-center mt-4">Rhyming Words</h1>
+      <form onSubmit={handleRhymingSubmit} className="p-4 text-center">
+        <input
+          type="text"
+          placeholder="Enter search term"
+          value={searchTerm1}
+          onChange={(event) => setSearchTerm1(event.target.value)}
+          className="bg-gray-200 rounded p-2"
+        />
+        <button type="submit" className="m-2 p-2 bg-blue-200 rounded-lg">
+          Search
+        </button>
+      </form>
+      <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4">
+        {rhymingWords.map((word: Word) => (
+          <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
+            {word.word}
+          </div>
+        ))}
+      </div>
+      <h1 className="text-lg font-bold text-center mt-4">Adjective Words</h1>
+      <form onSubmit={handleAdjSubmit} className="p-4 text-center">
+        <input
+          type="text"
+          placeholder="Enter search term"
+          value={searchTerm2}
+          onChange={(event) => setSearchTerm2(event.target.value)}
+          className="bg-gray-200 rounded p-2"
+        />
+        <button type="submit" className="m-2 p-2 bg-blue-200 rounded-lg">
+          Search
+        </button>
+      </form>
+      <div className="grid grid-cols-3 md:grid-cols-6 w-auto gap-4">
+        {adjWords.map((word: Word) => (
+          <div key={word.word} className="flex items-center justify-center h-10 bg-gray-400 rounded-md">
+            {word.word}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WordsPage;
